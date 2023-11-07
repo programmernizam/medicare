dfe8365c50bd1d809d7e1f3be964e084b594c2d8
programmerNizam <mdnizam12109@gmail.com>
2023-11-07 16:01:12 +0600

    [Done] Navbar Computer version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,54 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 const Header = () => {
+  const navLinks = [
+    {
+      path: "/home",
+      display: "Home",
+    },
+    {
+      path: "/doctors",
+      display: "Find a Doctor",
+    },
+    {
+      path: "/services",
+      display: "Services",
+    },
+    {
+      path: "/contact",
+      display: "Contact",
+    },
+  ];
   return (
-    <div>
-      Header
-    </div>
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* =========Logo========= */}
+          <div>
+            <img src={logo} alt="Logo" />
+          </div>
+          {/* ==========Menu========== */}
+          <div className="navigation">
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-semibold"
+                        : "text-textColor text-[16px] leading-7 font-medium"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
