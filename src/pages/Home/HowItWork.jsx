5df166d7b56fffca4404abe3a24b74c887774b55
programmerNizam <mdnizam12109@gmail.com>
2023-11-08 14:44:28 +0600

    How it work top section complete

--- src/pages/Home/HowItWork.jsx
+++ src/pages/Home/HowItWork.jsx
@@ -2,11 +2,18 @@ const HowItWork = () => {
   return (
     <section>
       <div className="container">
+        {/* =====Top Section==== */}
         <div className="lg:w-[470px] mx-auto">
           <h2 className="heading text-center">
             Providing the best medical services
           </h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health system offers unmatched,
+            expert health care.
+          </p>
         </div>
+        {/* =====Bottom Section===== */}
+        <div></div>
       </div>
     </section>
   );
