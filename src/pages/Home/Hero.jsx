a75498e01c96776de136a3b7006674066308768b
programmerNizam <mdnizam12109@gmail.com>
2023-11-08 14:00:59 +0600

    Hero section v1 completed

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,3 +1,6 @@
+import heroImg01 from "../../assets/images/hero-img01.png";
+import heroImg02 from "../../assets/images/hero-img02.png";
+import heroImg03 from "../../assets/images/hero-img03.png";
 const Hero = () => {
   return (
     <section className="hero_section pt-[60px] 2xl:h-[800px]">
@@ -16,6 +19,44 @@ const Hero = () => {
                 consequuntur! Repellendus repellat similique temporibus.
               </p>
               <button className="btn">Request an Appointment</button>
+            </div>
+            {/* =====Hero Counter====== */}
+            <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+              <div>
+                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] text-headingColor font-bold">
+                  30+
+                </h2>
+                <span className="w-100px h-2 bg-yellowColor rounded-full block -mt-[14px]"></span>
+                <p className="text_para">Years Of Experience</p>
+              </div>
+              <div>
+                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] text-headingColor font-bold">
+                  15+
+                </h2>
+                <span className="w-100px h-2 bg-purpleColor rounded-full block -mt-[14px]"></span>
+                <p className="text_para">Clinic Location</p>
+              </div>
+              <div>
+                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] text-headingColor font-bold">
+                  100%
+                </h2>
+                <span className="w-100px h-2 bg-irisBlueColor rounded-full block -mt-[14px]"></span>
+                <p className="text_para">Patient Satisfaction</p>
+              </div>
+            </div>
+          </div>
+          {/* ========Hero Image======= */}
+          <div className="flex gap-[30px] justify-end">
+            <div>
+              <img className="w-full" src={heroImg01} alt="Hero Image" />
+            </div>
+            <div className="mt-[30px]">
+              <img
+                className="w-full mb-[30px]"
+                src={heroImg02}
+                alt="Hero Image"
+              />
+              <img className="w-full" src={heroImg03} alt="Hero Image" />
             </div>
           </div>
         </div>
