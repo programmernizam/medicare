8927a81c16333127e2a9ddf300632008f7809018
programmerNizam <mdnizam12109@gmail.com>
2023-11-08 14:40:18 +0600

    Home page how it work section start

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import HowItWork from "./HowItWork";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <HowItWork />
     </div>
   );
 };
---
+++ src/pages/Home/HowItWork.jsx
@@ -0,0 +1,15 @@
+const HowItWork = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="lg:w-[470px] mx-auto">
+          <h2 className="heading text-center">
+            Providing the best medical services
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWork;
