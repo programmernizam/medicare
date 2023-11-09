b238eeba6a48b410781c572bd7028f643a67b086
programmerNizam <mdnizam12109@gmail.com>
2023-11-09 14:01:03 +0600

    Service top section completed

---
+++ src/components/Services/ServiceList.jsx
@@ -0,0 +1,5 @@
+const ServiceList = () => {
+  return <div></div>;
+};
+
+export default ServiceList;
--- src/pages/Home/ServiceSection.jsx
+++ src/pages/Home/ServiceSection.jsx
@@ -1,10 +1,19 @@
+import ServiceList from "../../components/Services/ServiceList";
+
 const ServiceSection = () => {
   return (
     <section>
       <div className="container">
+        {/* ====Service Top==== */}
         <div className="xl:w-[470px] mx-auto">
-            
+          <h2 className="heading text-center">Our Medical Services</h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health system offers unmatched,
+            expert health care.
+          </p>
         </div>
+        {/* =====Service List==== */}
+        <ServiceList />
       </div>
     </section>
   );
