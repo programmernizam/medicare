ef83128ec0b9d2ea8e53710bfb5071fa18f6bf1e
programmerNizam <mdnizam12109@gmail.com>
2023-11-09 14:06:29 +0600

    Service list component done

---
+++ src/components/Services/ServiceCard.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+const ServiceCard = ({ item, index }) => {
+  const { name, desc, bgColor, textColor } = item;
+  return <div className=""></div>;
+};
+
+export default ServiceCard;
