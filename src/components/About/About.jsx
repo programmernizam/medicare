9f9c4bebd0cd926fced89b7bc782dabe2a8dbe76
programmerNizam <mdnizam12109@gmail.com>
2023-11-09 13:44:18 +0600

    About Img complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,21 @@
+import aboutCardImg from "../../assets/images/about-card.png";
+import aboutImg from "../../assets/images/about.png";
 const About = () => {
-    return (
-        <section>
-            <div className="container">
-                about
+  return (
+    <section>
+      <div className="container">
+        <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+          {/* =====About Image===== */}
+          <div className="w-3/4 relative lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+            <img src={aboutImg} alt="" />
+            <div className="w-[200px] md:[300px] absolute bottom-4 z-20">
+              <img src={aboutCardImg} alt="" />
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
