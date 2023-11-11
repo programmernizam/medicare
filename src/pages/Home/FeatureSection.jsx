ac8c1697a66e5c589a646f0303ba6955fc41f7ec
programmerNizam <mdnizam12109@gmail.com>
2023-11-11 14:22:16 +0600

    Feature top start building and heading added

--- src/pages/Home/FeatureSection.jsx
+++ src/pages/Home/FeatureSection.jsx
@@ -2,7 +2,14 @@ const FeatureSection = () => {
   return (
     <section>
       <div className="container">
-        <div></div>
+        <div className="flex items-center justify-between flex-col lg:flex-row">
+          {/* Feature Content */}
+          <div className="xl:w-[670px]">
+            <h2>
+              Get Virtual Treatment <br /> anytime{" "}
+            </h2>
+          </div>
+        </div>
       </div>
     </section>
   );
