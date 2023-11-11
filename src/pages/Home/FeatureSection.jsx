800c0c9d6b6a09a88581dd350600ee6142f72fc8
programmerNizam <mdnizam12109@gmail.com>
2023-11-11 15:47:05 +0600

    Feature img section sub column work start

--- src/pages/Home/FeatureSection.jsx
+++ src/pages/Home/FeatureSection.jsx
@@ -29,6 +29,21 @@ const FeatureSection = () => {
           {/* ======Feature Image====== */}
           <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
             <img src={featureImg} className="w-3/4" alt="" />
+            <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:px-4 lg:pb-[26px] rounded-[10px]">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-[6px] lg:gap-3">
+                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-semibold">
+                    Tue, 24
+                  </p>
+                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-semibold">
+                    10:00
+                  </p>
+                </div>
+                <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 ;g:px-[9px]">
+                  <img src="" alt="" />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
