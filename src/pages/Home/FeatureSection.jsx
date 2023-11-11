9667f1418f8ca45ea2c15053bef5a04b88285047
programmerNizam <mdnizam12109@gmail.com>
2023-11-11 15:34:07 +0600

    Feature content column complete

--- src/pages/Home/FeatureSection.jsx
+++ src/pages/Home/FeatureSection.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const FeatureSection = () => {
   return (
     <section>
@@ -5,9 +7,24 @@ const FeatureSection = () => {
         <div className="flex items-center justify-between flex-col lg:flex-row">
           {/* Feature Content */}
           <div className="xl:w-[670px]">
-            <h2>
-              Get Virtual Treatment <br /> anytime{" "}
+            <h2 className="heading">
+              Get virtual treatment <br /> anytime
             </h2>
+            <ul>
+              <li className="text_para">
+                1. Schedule the appointment directly.
+              </li>
+              <li className="text_para">
+                2. Search your physicians here, and contact there office.
+              </li>
+              <li className="text_para">
+                3. View your physicians who are accepting new patients, use the
+                online Scheduling tool to select an appointment time.
+              </li>
+            </ul>
+            <Link to={"/"}>
+              <button className="btn">Learn More</button>
+            </Link>
           </div>
         </div>
       </div>
