4b0d4d9c2471e60d4e62024533955a7e1666ec04
programmerNizam <mdnizam12109@gmail.com>
2023-11-13 00:25:23 +0600

    Testimonail Section top section created

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,11 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+const Testimonial = () => {
+  return <div></div>;
+};
+
+export default Testimonial;
--- src/pages/Home/TestimonialSection.jsx
+++ src/pages/Home/TestimonialSection.jsx
@@ -1,5 +1,17 @@
 const TestimonialSection = () => {
-  return <div>testimonial</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className="heading text-center">What our patient says</h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health system offers unmatched,
+            expert health care.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TestimonialSection;
