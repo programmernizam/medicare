abbb0dd1a8419b529579a2f255e3fef29e101e7b
programmerNizam <mdnizam12109@gmail.com>
2023-11-12 23:25:36 +0600

    Faq image added

--- src/pages/Home/FaqSection.jsx
+++ src/pages/Home/FaqSection.jsx
@@ -1,5 +1,16 @@
+import faqImage from "../../assets/images/faq-img.png";
 const FaqSection = () => {
-  return <div>FAQS</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="flex justify-between items-center gap-[50px] lg:gap-0">
+          <div className="hidden w-1/2 md:block">
+            <img src={faqImage} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FaqSection;
