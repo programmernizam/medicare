5e3840933d50285d42e257bdcf2c7082c0cd22f9
programmerNizam <mdnizam12109@gmail.com>
2023-11-14 19:56:27 +0600

    Doctor page Doctor list added

--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,20 +1,33 @@
+import doctors from "../../assets/data/doctors";
+import DoctorCard from "../../components/Doctors/DoctorCard";
 const Doctors = () => {
   return (
-    <section className="bg-[#fff9ea]">
-      <div className="container text-center">
-        <h2 className="heading">Find a Doctor</h2>
-        <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex justify-between items-center">
-          <input
-            type="search"
-            placeholder="Search Doctor"
-            className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
-          />
-          <button className="btn mt-0 rounded-[0px] rounded-r-md">
-            Search
-          </button>
+    <>
+      <section className="bg-[#fff9ea]">
+        <div className="container text-center">
+          <h2 className="heading">Find a Doctor</h2>
+          <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex justify-between items-center">
+            <input
+              type="search"
+              placeholder="Search Doctor"
+              className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+            />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">
+              Search
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
