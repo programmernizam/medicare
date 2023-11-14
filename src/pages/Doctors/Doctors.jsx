ebfecd96a3b1603b8a59843de7edb27db6b442f5
programmerNizam <mdnizam12109@gmail.com>
2023-11-14 19:26:07 +0600

    Doctor Page search input fild created

--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,7 +1,16 @@
 const Doctors = () => {
   return (
-    <section>
-      <div className="container text-center"></div>
+    <section className="bg-[#fff9ea]">
+      <div className="container text-center">
+        <h2 className="heading">Find a Doctor</h2>
+        <div className="max-w-[570pxy] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex justify-between items-center">
+          <input
+            type="search"
+            placeholder="Search Doctor"
+            className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+          />
+        </div>
+      </div>
     </section>
   );
 };
