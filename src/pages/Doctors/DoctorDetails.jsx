b15fa275af80937679e45b9f74adc5e7b0a33cc6
programmerNizam <mdnizam12109@gmail.com>
2023-11-15 19:12:18 +0600

    Doctor Details page image added

--- src/pages/Doctors/DoctorDetails.jsx
+++ src/pages/Doctors/DoctorDetails.jsx
@@ -1,7 +1,18 @@
+import doctorImg from "../../assets/images/doctor-img02.png";
 const DoctorDetails = () => {
   return (
     <section>
-      <div className="max-w-[1170px] px-5 mx-auto"></div>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-[50px]">
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-5">
+              <figure className="max-w-[200px] max-h-[200px]">
+                <img src={doctorImg} alt="" className="w-full" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
