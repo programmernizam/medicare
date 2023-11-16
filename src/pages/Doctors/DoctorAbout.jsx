912281c063934f32406a62f2e3b570e2ecd4cbb2
programmerNizam <mdnizam12109@gmail.com>
2023-11-16 23:00:46 +0600

    Doctor about component heading added

--- src/pages/Doctors/DoctorAbout.jsx
+++ src/pages/Doctors/DoctorAbout.jsx
@@ -1,5 +1,22 @@
 const DoctorAbout = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About of
+          <span className="text-[24px] text-irisBlueColor font-bold leading-9">
+            MD. Nizam Uddin
+          </span>
+        </h3>
+        <p className="text_para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque beatae
+          explicabo consequuntur deleniti voluptatem eveniet quam debitis sint
+          a! Dignissimos cumque eaque facilis reiciendis nemo quisquam eligendi
+          cum quibusdam fuga?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default DoctorAbout;
