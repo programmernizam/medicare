2afb39c6ba74f2939be7a98768b35bbfb64d512e
programmerNizam <mdnizam12109@gmail.com>
2023-11-16 23:07:18 +0600

    Doctor Education Section start building

--- src/pages/Doctors/DoctorAbout.jsx
+++ src/pages/Doctors/DoctorAbout.jsx
@@ -15,6 +15,26 @@ const DoctorAbout = () => {
           cum quibusdam fuga?
         </p>
       </div>
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                23 June, 2008
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
