206cf34fe4652dc5e50db8ffa47f2acfaa93e4e8
programmerNizam <mdnizam12109@gmail.com>
2023-11-18 04:05:40 +0600

    Feedback Heading added

--- src/pages/Doctors/DoctorFeedback.jsx
+++ src/pages/Doctors/DoctorFeedback.jsx
@@ -1,7 +1,11 @@
 const DoctorFeedback = () => {
   return (
     <div>
-      <div></div>
+      <div className="mb-[50px]">
+        <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
+          All reviews (274)
+        </h4>
+      </div>
     </div>
   );
 };
