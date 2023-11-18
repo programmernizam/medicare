de8596bb5e4e00696a5dd76d165c4171200d2dff
programmerNizam <mdnizam12109@gmail.com>
2023-11-19 01:55:44 +0600

    Start building contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,20 @@
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className="text-center heading">Contact Us</h2>
+        <p className="mb-8 lg:mb-16 text-center font-light text_para">
+          Got a technical issue? Want to send us about beta feature? Let us
+          know.
+        </p>
+        <form action="">
+          <label htmlFor="email" className="form_label">
+            Your Email
+          </label>
+          <input type="email" id="email" placeholder="example@gmail.com" className="form_input mt-1" />
+        </form>
+      </div>
+    </section>
   );
 };
 
