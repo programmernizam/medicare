a4db674fbd331100e5cbdb64068f8cdc74b57153
programmerNizam <mdnizam12109@gmail.com>
2023-11-19 01:02:08 +0600

    Signup page image box added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,17 @@
+import signUpImg from "../assets/images/signup.gif";
 const SignUp = () => {
   return (
     <section className="px-5 lg:px-0">
       <div className="max-w-[1170px] mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2"></div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {/* Image box */}
+          <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+            <figure className="rounded-l-lg">
+              <img src={signUpImg} alt="" className="w-full rounded-l-lg" />
+            </figure>
+          </div>
+          {/* Signup Form */}
+        </div>
       </div>
     </section>
   );
