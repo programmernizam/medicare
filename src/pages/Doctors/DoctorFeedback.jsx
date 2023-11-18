4ffa961571d0a740f5466c1965b9f9c29200e802
programmerNizam <mdnizam12109@gmail.com>
2023-11-18 18:48:44 +0600

    Feedback author avatar and name added

--- src/pages/Doctors/DoctorFeedback.jsx
+++ src/pages/Doctors/DoctorFeedback.jsx
@@ -1,3 +1,4 @@
+import avatarImg from "../../assets/images/avatar-icon.png";
 const DoctorFeedback = () => {
   return (
     <div>
@@ -5,6 +6,18 @@ const DoctorFeedback = () => {
         <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
           All reviews (274)
         </h4>
+        <div className="flex justify-between gap-10 mb-[30px]">
+          <div className="flex gap-3">
+            <figure className="w-10 h-10 rounded-full">
+              <img src={avatarImg} alt="" className="w-full" />
+            </figure>
+            <div>
+              <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
+                Rasel Mia
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
