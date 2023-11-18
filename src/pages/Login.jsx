84713d75698dce210a86a4bc442f4ff5bb36729a
programmerNizam <mdnizam12109@gmail.com>
2023-11-19 00:47:28 +0600

    Login form email input added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,11 @@ import { useState } from "react";
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
   });
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <section className="px-5 lg:px-0">
       <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
@@ -18,6 +21,8 @@ const Login = () => {
               placeholder="Enter Your Email"
               name="email"
               value={formData.email}
+              onChange={handleInputChange}
+              className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor:pointer"
             />
           </div>
         </form>
