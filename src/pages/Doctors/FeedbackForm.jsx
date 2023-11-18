97f2852ed93922077d7f5eb4768da45cf6c4adb8
programmerNizam <mdnizam12109@gmail.com>
2023-11-18 22:02:31 +0600

    Added Feedback form star icons

--- src/pages/Doctors/FeedbackForm.jsx
+++ src/pages/Doctors/FeedbackForm.jsx
@@ -1,5 +1,26 @@
+import { AiFillStar } from "react-icons/ai";
 const FeedbackForm = () => {
-  return <div>Feedback Form</div>;
+  return (
+    <form action="">
+      <div>
+        <h3 className="text-[16px] leading-6 text-headingColor font-semibold mb-4">
+          How would you rate the overall experience?
+        </h3>
+        <div>
+          {[...Array(5).keys()].map((_, index) => {
+            index += 1;
+            return (
+              <button key={index}>
+                <span>
+                  <AiFillStar />
+                </span>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default FeedbackForm;
