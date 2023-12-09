66a330f1bec5c53e7b9163a22d856d874ff1a80e
programmerNizam <mdnizam12109@gmail.com>
2023-12-09 16:07:17 +0600

    auth Context created

---
+++ src/context/authContext.js
@@ -0,0 +1,24 @@
+import { createContext } from "react"
+
+const initialState = {
+    user: null,
+    role: null,
+    token: null,
+}
+const authContext = createContext(initialState)
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN_START':
+            return {
+                user: null,
+                role: null,
+                token: null
+            }
+        case 'LOGIN_SUCCESS':
+            return {
+                user: action.payload.user,
+                token: action.payload.token,
+                role: action.payload.role
+            }
+    }
+}
