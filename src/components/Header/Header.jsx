288177c952fe009d245b0b81c6dcbcc8411e273a
programmerNizam <mdnizam12109@gmail.com>
2023-12-11 01:55:03 +0600

    Add conditional logic between login button and user avater

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useRef } from "react";
 import { BiMenu } from "react-icons/bi";
 import { Link, NavLink } from "react-router-dom";
-import userImg from "../../assets/images/avatar-icon.png";
 import logo from "../../assets/images/logo.png";
 import { authContext } from "../../context/AuthContext";
 
@@ -80,23 +79,32 @@ const Header = () => {
           </div>
           {/* =========Nav Right========= */}
           <div className="flex items-center gap-4">
-            <div className="hidden">
-              <Link>
-                <figure className="w-[35px] h-[35px] rounded-full cursor-auto">
-                  <img
-                    src={userImg}
-                    className="w-full rounded-full"
-                    alt="User Image"
-                  />
-                </figure>
+            {token && user ? (
+              <div>
+                <Link
+                  to={`${
+                    role === "doctor"
+                      ? "/doctor/profile/me"
+                      : "/users/profile/me"
+                  }`}
+                >
+                  <figure className="w-[35px] h-[35px] rounded-full cursor-auto">
+                    <img
+                      src={user?.photo}
+                      className="w-full rounded-full h-full"
+                      alt="User Image"
+                    />
+                  </figure>
+                </Link>
+              </div>
+            ) : (
+              <Link to="/login">
+                <button className="bg-primaryColor font-semibold py-2 px-6 text-white h-[44px] flex items-center rounded-[50px]">
+                  Login
+                </button>
               </Link>
-            </div>
-            <h1>{user?.name}</h1>
-            <Link to="/login">
-              <button className="bg-primaryColor font-semibold py-2 px-6 text-white h-[44px] flex items-center rounded-[50px]">
-                Login
-              </button>
-            </Link>
+            )}
+
             <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className="w-6 h-6 cursor-pointer" />
             </span>
