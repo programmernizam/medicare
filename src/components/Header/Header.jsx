6552c1b8d025f1923b3e2aedc678ed3b867ef154
programmerNizam <mdnizam12109@gmail.com>
2023-12-11 02:05:55 +0600

    Login functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -95,7 +95,6 @@ const Header = () => {
                       alt="User Image"
                     />
                   </figure>
-                  <h2>{user?.name}</h2>
                 </Link>
               </div>
             ) : (
