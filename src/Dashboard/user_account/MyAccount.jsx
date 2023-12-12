a6194e4691327accde7151f195449cdb7962bcba
programmerNizam <mdnizam12109@gmail.com>
2023-12-13 00:14:59 +0600

    my account page design starting

--- src/Dashboard/user_account/MyAccount.jsx
+++ src/Dashboard/user_account/MyAccount.jsx
@@ -1,9 +1,11 @@
 const MyAccount = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="max-w-[1170px] px-5 mx-auto'">
+      <div className="grid md:grid-cols-3 gap-10">
+        <div className="pb-[50px] px-[30px] rounded-md"></div>
+      </div>
+    </div>
+  );
 };
 
 export default MyAccount;
