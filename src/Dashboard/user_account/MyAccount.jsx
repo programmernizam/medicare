56b902ad4708e799e35858585c5c49b55a2e26e4
programmerNizam <mdnizam12109@gmail.com>
2023-12-13 00:25:01 +0600

    Avater added

--- src/Dashboard/user_account/MyAccount.jsx
+++ src/Dashboard/user_account/MyAccount.jsx
@@ -1,8 +1,13 @@
+import userImg from "../../assets/images/doctor-img01.png";
 const MyAccount = () => {
   return (
     <div className="max-w-[1170px] px-5 mx-auto'">
       <div className="grid md:grid-cols-3 gap-10">
-        <div className="pb-[50px] px-[30px] rounded-md"></div>
+        <div className="pb-[50px] px-[30px] rounded-md">
+          <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+            <img src={userImg} alt="" className="w-full h-full rounded-full" />
+          </figure>
+        </div>
       </div>
     </div>
   );
