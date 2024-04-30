918e0e83df33c734a119d7da12f900db6575b5ee
programmerNizam <mdnizam12109@gmail.com>
2024-05-01 01:59:58 +0600

    [Update] Server Api url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://medicare-backend-7g2u.vercel.app/api/v1"
+export const BASE_URL = "https://medicare-backend-rqbn.onrender.com/api/v1"
 export const token = localStorage.getItem("token")
